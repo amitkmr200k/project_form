4a19dd0c9e1fd7dca9104fa50e3933c860dbdb17
mindfire <mindfire@mindfire-nj7>
2016-02-01 11:16:22 +0530

    test.php removed

--- project/test.php
+++
@@ -1,7 +0,0 @@
-<?php
-include("connection.php");
-$query="SELECT COUNT(*) AS abc from user where email_id={$_POST['email_id']}";
-$result=mysqli_query($connection,$query);
-$row=mysqli_fetch_assoc($result);
-echo $row['abc'];
-?>
